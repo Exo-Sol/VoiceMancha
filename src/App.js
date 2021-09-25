b08633066fb7d917e72cc6ca4b0a902b4dd70ae8
Exo Sol <1.petarzlatar@gmail.com>
2021-09-25 19:59:00 +0200

    added chart, next instead of chart total sum, if clicked chart, and also style chart

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import SpeechRecognition, {
 } from "react-speech-recognition";
 import { useState, useEffect } from "react";
 import { TimeStamp } from "./TimeStamp";
+
 import styles from "./css/main.module.scss";
+import blackMic from "./icons/mic-black.png";
+import whiteMic from "./icons/mic-white.png";
+import clear from "./icons/clear.png";
+
+import { Bar } from "react-chartjs-2";
 
 const Dictaphone = () => {
   const pullData = () => {
@@ -124,31 +130,79 @@ const Dictaphone = () => {
   }, []);
 
   return (
-    <div className={styles.mainDiv}>
-      <div className={styles.column1}>
-        <div className={styles.manchaList}>
-          <ul>
-            {mancha.map((e) => (
-              <li key={e.id}>
-                {e.manch}{" "}
-                {edit && <button onClick={() => deleteEntry(e.id)}>x</button>}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-
-      <div className={styles.column2}>
+    <div className={listening ? styles.mainDivRec : styles.mainDiv}>
+      <>
         <div className={styles.micAndTranscript}>
           <p className={styles.micIndi}>
             Microphone: {listening ? "on" : "off"}
           </p>
-          <p>{transcript ? transcript : "$$"}</p>
+          <p id={styles.transcript}>{transcript ? transcript : "$$"}</p>
         </div>
+      </>
+      <Bar
+        className={styles.manchaList}
+        data={{
+          labels: mancha.map((e) => {
+            return `${e.timeObj.hour}.${e.timeObj.minutes}`;
+          }),
+          datasets: [
+            {
+              label: "KN",
+              data: mancha.map((e) => {
+                return e.manch;
+              }),
+              backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        }}
+        height={"200px"}
+      />
 
-        <button onClick={record}>Start</button>
+      {/* <div className={styles.manchaList}>
+        <ul>
+          {mancha.map((e) => (
+            <li key={e.id}>
+              {e.manch}{" "}
+              {edit && <button onClick={() => deleteEntry(e.id)}>x</button>}
+            </li>
+          ))}
+        </ul>
+      </div> */}
+      <div className={styles.buttonsCont}>
+        <img
+          src={listening ? whiteMic : blackMic}
+          alt="Mic"
+          onClick={record}
+          className={styles.button}
+        ></img>
 
-        <button onClick={editIt}>Edit</button>
+        {listening ? (
+          <div className={styles.button}></div>
+        ) : (
+          <img
+            src={clear}
+            alt="clear"
+            onClick={editIt}
+            className={styles.button}
+            style={{ opacity: "0.8" }}
+          ></img>
+        )}
       </div>
     </div>
   );
