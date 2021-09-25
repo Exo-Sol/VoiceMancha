ae53c21fa5df0ac55a80b460eb25e9ef3a5d51ce
Exo Sol <1.petarzlatar@gmail.com>
2021-09-25 20:59:23 +0200

    changed mainDiv UI based on the clicks, next migrate some code to components and show x only when in list

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import blackMic from "./icons/mic-black.png";
 import whiteMic from "./icons/mic-white.png";
 import clear from "./icons/clear.png";
 
-import { Bar } from "react-chartjs-2";
+import BarChart from "./components/Bar";
 
 const Dictaphone = () => {
   const pullData = () => {
@@ -29,6 +29,7 @@ const Dictaphone = () => {
   const [edit, setEdit] = useState(false);
   const [idToDelete, seIdToDelete] = useState(null);
   const [initialRender, setInitialRender] = useState(() => true);
+  const [mainDisplay, setMainDisplay] = useState(0);
 
   const record = () => {
     SpeechRecognition.startListening({ language: "hr_HR" });
@@ -51,6 +52,36 @@ const Dictaphone = () => {
 
     seIdToDelete(id);
     setMancha(newManchList);
+  };
+
+  const changeMainDisply = () => {
+    if (mainDisplay === 2) {
+      setMainDisplay(0);
+    } else {
+      setMainDisplay(mainDisplay + 1);
+    }
+  };
+
+  const renderSwitch = (mainDis) => {
+    switch (mainDis) {
+      case 0:
+        return "total";
+      case 1:
+        return <BarChart mancha={mancha} />;
+      case 2:
+        return (
+          <ul>
+            {mancha.map((e) => (
+              <li key={e.id}>
+                {e.manch}{" "}
+                {edit && <button onClick={() => deleteEntry(e.id)}>x</button>}
+              </li>
+            ))}
+          </ul>
+        );
+      default:
+        return <></>;
+    }
   };
 
   useEffect(() => {
@@ -139,51 +170,10 @@ const Dictaphone = () => {
           <p id={styles.transcript}>{transcript ? transcript : "$$"}</p>
         </div>
       </>
-      <Bar
-        className={styles.manchaList}
-        data={{
-          labels: mancha.map((e) => {
-            return `${e.timeObj.hour}.${e.timeObj.minutes}`;
-          }),
-          datasets: [
-            {
-              label: "KN",
-              data: mancha.map((e) => {
-                return e.manch;
-              }),
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-              ],
-              borderWidth: 1,
-            },
-          ],
-        }}
-        height={"200px"}
-      />
 
-      {/* <div className={styles.manchaList}>
-        <ul>
-          {mancha.map((e) => (
-            <li key={e.id}>
-              {e.manch}{" "}
-              {edit && <button onClick={() => deleteEntry(e.id)}>x</button>}
-            </li>
-          ))}
-        </ul>
-      </div> */}
+      <div className={styles.manchaList} onClick={changeMainDisply}>
+        {renderSwitch(mainDisplay)}
+      </div>
       <div className={styles.buttonsCont}>
         <img
           src={listening ? whiteMic : blackMic}
---
+++ src/components/Bar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+
+const BarChart = ({ mancha }) => {
+  const chartData = {
+    labels: mancha.map((e) => {
+      return `${e.timeObj.hour}.${e.timeObj.minutes}`;
+    }),
+    datasets: [
+      {
+        label: "KN",
+        data: mancha.map((e) => {
+          return e.manch;
+        }),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return <Bar data={chartData} height={"200px"} />;
+};
+
+export default BarChart;
